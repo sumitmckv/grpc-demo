275a3d871c28714fd26d4dc559bfcced3607e4d9
Ghosh <sumghosh@informatica.com>
2021-05-25 16:16:28 +0530

    refactor code and added client streaming

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const PROTO_PATH = __dirname + "/proto/user.proto";
+const PROTO_PATH = __dirname + "/user.proto";
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const packageDef = protoLoader.loadSync(PROTO_PATH, {
@@ -13,9 +13,27 @@ const user_proto = grpc.loadPackageDefinition(packageDef).user;
 
 function main() {
     const client = new user_proto.User("localhost:50051", grpc.credentials.createInsecure());
-    client.getUser({id: 1}, (err, res) => {
-        console.log("User details:", res.user);
-    })
+    const arg = parseInt(process.argv[2]);
+    switch(arg) {
+        // Server streaming RPC
+        case 2: {
+            const call = client.getUserStream({userIdList: [1,2,4]});
+            call.on("data", (response) => console.log(response));
+            call.on("end", () => console.log("Request completed!!"));
+            break;
+        }
+        // Client streaming RPC
+        case 3: {
+            const call = client.addUsersStream((_err, res) => console.log("Added users details:\n", res.users));
+            const newUsers = [{email: "test4@test.com", name: "Test4 Test4"}, {email: "test5@test.com", name: "Test5 Test5"}];
+            newUsers.forEach(user => call.write(user));
+            call.end();
+            break;
+        }
+        // Unary RPC
+        default:
+            client.getUser({id: 1}, (_err, res) => console.log("User details:", res.user));
+    }
 }
 
 main();
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PROTO_PATH = __dirname + "/proto/user.proto";
+const PROTO_PATH = __dirname + "/user.proto";
 const {users} = require("./data");
 
 const grpc = require("@grpc/grpc-js");
@@ -15,12 +15,42 @@ const user_proto = grpc.loadPackageDefinition(packageDef).user;
 
 /**
  * Implements the getUser RPC method.
+ * Unary RPC
  */
 function getUser(call, cb) {
     const user = users.find(u => u.id === call.request.id) || null;
     cb(null, {user});
 }
 
+/**
+ * Implements the getUserStream RPC method.
+ * Server streaming RPC
+ */
+ function getUserStream(call) {
+  const userIdList = call.request.userIdList;
+  userIdList.forEach(id => {
+      const user = users.find(u => u.id === id) || null;
+      if (user) {
+          call.write({response: `User details: Id ${id}, email ${user.email}, name ${user.name}`});
+      } else {
+          call.write({response: `No user exists with Id: ${id}`});
+      }
+  });
+  call.end();
+}
+
+/**
+ * Implements the addUsersStream RPC method.
+ * Server streaming RPC
+ */
+function addUsersStream(call, cb) {
+  let users = [];
+  call.on("data", (req) => {
+    req.id = Math.floor(Math.random() * 100) + 1;
+    users.push(req);
+  });
+  call.on("end", () => cb(null, {users}));
+}
 
 /**
  * Starts an RPC server that receives requests for the User service at the
@@ -28,7 +58,7 @@ function getUser(call, cb) {
  */
 function main() {
     var server = new grpc.Server();
-    server.addService(user_proto.User.service, {getUser});
+    server.addService(user_proto.User.service, {getUser, getUserStream, addUsersStream});
     server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
       server.start();
       console.log(`gRPC server has started`)
